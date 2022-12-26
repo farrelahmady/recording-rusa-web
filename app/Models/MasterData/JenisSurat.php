12d632683a2231c00de1af1e510f2b7a59deb630
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2022-12-27 01:44:14 +0700

    JenisSurat Model

--- app/Models/MasterData/JenisSurat.php
+++ app/Models/MasterData/JenisSurat.php
@@ -2,10 +2,35 @@
 
 namespace App\Models\MasterData;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Operational\Recording\Administrasi;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class JenisSurat extends Model
 {
     use HasFactory;
+
+    protected $table = 'jenis_surat';
+    protected $guarded = ['id'];
+    protected $primaryKey = 'id';
+    protected $keyType = 'int';
+    protected $casts = [
+        'id' => 'int',
+    ];
+
+    //* Attribute Accessors & Mutators
+    protected function jenis(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) =>  strtoupper($value),
+            set: fn ($value) =>  strtoupper($value),
+        );
+    }
+
+    //* Relationships
+    public function administrasi()
+    {
+        return $this->hasMany(Administrasi::class, 'id_jenis_surat');
+    }
 }
