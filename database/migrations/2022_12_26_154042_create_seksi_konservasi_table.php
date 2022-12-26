1af8f9627fe407da9a71ad4c9704cd7e0900bf3e
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2022-12-26 22:42:54 +0700

    seksi konservasi table

---
+++ database/migrations/2022_12_26_154042_create_seksi_konservasi_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seksi_konservasi', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('nama_depan_ketua', 50);
+            $table->string('nama_belakang_belakang', 50)->nullable();
+            $table->integer('wilayah');
+            $table->string('no_telp', 15);
+            $table->text('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seksi_konservasi');
+    }
+};
