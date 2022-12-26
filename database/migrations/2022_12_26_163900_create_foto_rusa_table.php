cdc26a96dc8de2b12dc08897f0615e688bfe495c
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2022-12-26 23:40:20 +0700

    foto rusa table and the foreign

---
+++ database/migrations/2022_12_26_163900_create_foto_rusa_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foto_rusa', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->foreignUuid('id_rusa')->constrained('rusa')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->text('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('foto_rusa', function (Blueprint $table) {
+            $table->dropForeign(['id_rusa']);
+        });
+        Schema::dropIfExists('foto_rusa');
+    }
+};
