64a9b15cd7490d6a08021054be34df476b74b8e3
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2022-12-27 04:22:22 +0700

    Seeders

--- app/Http/Controllers/API/PengurusController.php
+++ app/Http/Controllers/API/PengurusController.php
@@ -2,13 +2,44 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User\Pengurus;
+use Illuminate\Support\Facades\Validator;
 
 class PengurusController extends Controller
 {
     public function login(Request $request)
     {
+        $validation = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validation->fails()) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'errprs' => $validation->errors(),
+            ], 400);
+        }
+
+        $credentials = request(['email', 'password']);
+
+        if (!auth()->attempt($credentials)) {
+            return response()->json([
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+
+        $user = Pengurus::where('email', $request->email)->first();
+
+        return response()->json([
+            'message' => 'Authorized',
+            'data' => [
+                'user' => $user,
+                'token' => $user->createToken('pengurus')->plainTextToken,
+            ],
+        ], 200);
     }
 
     public function logout(Request $request)
---
+++ database/seeders/RusaSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RusaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
