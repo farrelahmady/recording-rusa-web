dac3a9250beb69a3c40c1cac4b6e8489460db7a0
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2022-12-27 01:54:55 +0700

    RolePengurusPermission

--- app/Models/ManagementAccess/RolePengurusPermission.php
+++ app/Models/ManagementAccess/RolePengurusPermission.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class RolePengurusPermission extends Model
 {
     use HasFactory;
+
+    protected $table = 'rolepengurus_permission';
+
+    protected $guarded = ['id'];
+    protected $primaryKey = 'id';
+    protected $keyType = 'int';
+    protected $casts = [
+        'id' => 'int',
+    ];
+
+    //* Relationships
+    public function rolePengurus()
+    {
+        return $this->belongsTo(RolePengurus::class, 'id_role_pengurus');
+    }
+
+    public function permission()
+    {
+        return $this->belongsTo(Permission::class, 'id_permission');
+    }
 }
