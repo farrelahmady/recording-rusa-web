5897d4d6f5a66e3ca199f44c945c5481d64594d2
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2022-12-26 22:30:12 +0700

    pemilik table

---
+++ database/migrations/2022_12_26_152024_create_pemilik_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pemilik', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('nama_depan', 50);
+            $table->string('nama_belakang', 50)->nullable();
+            $table->string('no_telp', 15);
+            $table->text('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pemilik');
+    }
+};
