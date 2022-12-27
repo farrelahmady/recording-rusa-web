97640a3ac390843de5bacc7c28dbe17c9a975711
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2022-12-27 22:01:50 +0700

    Website Route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RusaController;
+use App\Http\Controllers\PengurusController;
+use App\Http\Controllers\KesehatanController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\PenangkaranController;
+use App\Http\Controllers\AdministrasiController;
+use App\Http\Controllers\PemilikController;
+use App\Http\Controllers\RolePengurusController;
+use App\Http\Controllers\SeksiKonservasiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +24,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::resources([
+    "administrasi" => AdministrasiController::class,
+    "kesehatan" => KesehatanController::class,
+    "penangkaran" => PenangkaranController::class,
+    "pengurus" => PengurusController::class,
+    "permission" => PermissionController::class,
+    "pengurus/role" => RolePengurusController::class,
+    "rusa" => RusaController::class,
+]);
+
+Route::prefix("pemilik")->controller(PemilikController::class)->group(function () {
+    Route::get("login");
+    Route::post("login");
+    Route::post("logout");
+});
+Route::prefix("seksikonservasi")->controller(SeksiKonservasiController::class)->group(function () {
+    Route::get("login");
+    Route::post("login");
+    Route::post("logout");
 });
